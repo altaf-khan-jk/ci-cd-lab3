512309ee7daf555cab232b72fbe7e73c711a496f
Altaf Khan <Akhan8508@conestogac.on.ca>
2025-11-27 16:45:31 -0500

    Test pipeline trigger: update message

--- lambda/index.js
+++ lambda/index.js
@@ -2,7 +2,7 @@ exports.handler = async function(event) {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: process.env.MESSAGE,
+      message: process.env.MESSAGE || 'Hello from Lambda!',
       input: event
     })
   };
